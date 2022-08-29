081a3bf207087706cb82d69bdfcdd87ceace5d1b
carlos5G <danielgue335@gmail.com>
2022-08-29 10:27:12 -0500

    Agrega el proyecto

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+   document.querySelector(".nav-menu").classList.toggle("show");
+ });
+ 
+ ScrollReveal().reveal('.showcase', { delay: 500 });
+ ScrollReveal().reveal('.news-cards', { delay: 500 });
+ ScrollReveal().reveal('.banner-one', { delay: 500 });
+ ScrollReveal().reveal('.social', { delay: 500 });
+ 
+ 
